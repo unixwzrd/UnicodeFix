f1d72a707dd1e18ed980cf79fcbb3070d6a468a5
M S <unixwzrd.register@mac.com>
2025-11-15 07:42:32 -0600

    202511115_01-Update - Minor typo changes and TOML file update.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-$!/bin/bash
+#!/usr/bin/env bash
 
 python -m venv venv
 source venv/bin/activate
@@ -14,7 +14,7 @@ fi
 # Source the .bashrc file to add the necessary items to the PATH
 source ~/.bashrc
 
-echo "You will need to install teh package using pip as described in the README.md file. Additional information is in the README.md file for more features of the package."
+echo "You will need to install the package using pip as described in the README.md file. Additional information is in the README.md file for more features of the package."
 echo ""
 echo "Quick Start - simply run the following command and it will install the package and make it available to use:"
 echo "pip install ."
@@ -22,6 +22,6 @@ echo ""
 echo "If you wish to do development, or want to use the package in your own projects, use the following command:"
 echo "pip install -e ."
 echo ""
-echo "if you wish to use teh optional NLTK analytics, install the following optional extras for current and future NLP metrics:"
+echo "If you wish to use the optional NLTK analytics, install the following optional extras for current and future NLP metrics:"
 echo "pip install .[nlp]"
 echo ""
